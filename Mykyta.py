1d65bf978aecea7cc0b4872afb2168fa88e6f8d4
Arthut Shmaltsell <shmaltsell@gmail.com>
2021-03-29 03:43:42 +0300

    Hello world

---
+++ Mykyta.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+import time
+api_id = "3774678"
+api_hash = "ac882a54f7884879a6f794ff33c1fb2b"
+phone_number = "+380500298007"
+
+
+print("start")
+while True:
+    mins = time.strftime("%M");
+    if int(mins) == 46:
+        print('sending...')
+        with Client("Shmaltsell", api_id, api_hash, phone_number = phone_number) as app:
+            app.send_message(-306597811,"/drochnut")
+        time.sleep(120)
+        print("have sent")
---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python Mykyta.py
